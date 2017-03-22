1a63a2a70f4f86cb691a2b200e90d5065a806378
Trd Laz <tradedd@trds-mbp.wifi.lagcc>
2017-03-22 16:49:33 -0700

    uodate for memory game

---
+++ main.js
@@ -0,0 +1,12 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+if (cardOne === cardThree){
+    console.log(card = true);
+} else {
+    console.log(card = false);
+}
+    
